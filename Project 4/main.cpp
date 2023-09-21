803a86261021e05e0fed4048c3582df534e1c0da
alexmarthan <145699965+alexmarthan@users.noreply.github.com>
2023-09-22 00:46:16 +0800

    Add files via upload

---
+++ Project 4/main.cpp
@@ -0,0 +1,186 @@
+//
+//  main.cpp
+//  HW4
+//
+//  Created by Xela Xela on 17/2/2023.
+
+#include <iostream>
+#include <string>
+#include <cassert>
+using namespace std;
+
+int locateMaximum( const  string  array[ ],  int  n ){
+    string ValueAtIndex, MaximumValue;
+    int IndexOfMax = -1;
+    
+    for (int i = 0; i < n; i++){
+        ValueAtIndex = array[i];
+        if (ValueAtIndex > MaximumValue){
+            MaximumValue = ValueAtIndex;
+            IndexOfMax = i;
+        }
+
+    }
+    return IndexOfMax;
+}
+
+bool hasNoDuplicates( const string array[ ], int  n ){
+    bool returnVal = 1;
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; ++j){
+            if (i!=j){
+                if (array[i] == array[j]){
+                    returnVal = 0;
+                    break;
+                }
+            }
+        }
+    }
+    if (n < 0){
+        returnVal = 0;
+    }
+    return returnVal;
+}
+
+
+int countXPairs( const string array[ ], int  n, int x ){
+    int counter = 0;
+    
+    for (int i = 0; i < n; i++){
+        if (array[i] >= "0" && array[i] < ":" ){ // array must only contain numbers
+            for (int j = 0; j < n; j++){
+                if (stoi(array[i]) + stoi(array[j]) == x && (i != j)){
+                    counter++;
+                }
+            }
+        }
+        else {
+            break;
+        }
+    }
+    
+    int returnValue = counter/2; // everything is counted twice (for when i and j values are switched)
+    if (n<=0){
+        returnValue = -1;
+    }
+    
+    return returnValue;
+}
+
+int findSecondToLastOccurrence( const string array[ ], int n, string target ){
+    
+    int returnValue = -1;
+    
+    int firstCounter(0);
+    for (int index = 0; index < n; index++){ // this figures out how many "target"s we have
+        if (array[index] == target){
+            firstCounter++;
+        }
+    }
+    int counter2(0);
+    for (int i = 0; i < n; i++){
+        if (array[i] == target){
+            counter2++;
+        }
+        if (firstCounter > 1){
+            if (counter2 == firstCounter - 1){
+                returnValue = i;
+                break;
+            }
+        }
+    }
+    
+    return returnValue;
+}
+
+int countPunctuation( const string array[ ], int   n ){
+    int punctCounter(0);
+    int returnValue(-1);
+    for (int index = 0; index < n; index++){
+        for (size_t charIndex = 0; charIndex < array[index].size(); charIndex++){
+            if ( array[index][charIndex] == '.' || array[index][charIndex] == ',' || array[index][charIndex] == '!' || array[index][charIndex] == ';' || array[index][charIndex] == '\'' || array[index][charIndex] == '-' || array[index][charIndex] == '/' || array[index][charIndex] == ':' || array[index][charIndex] == '?' || array[index][charIndex] == '"'){
+                punctCounter++;
+            }
+        }
+    }
+    returnValue = punctCounter;
+    
+    if (n <= 0){
+        returnValue = -1;
+    }
+    return returnValue;
+}
+
+int flipAround( string array[ ], int n ){
+        
+    for (int i = 0; i < (n/2); i++){ // til n/2 so that it doesnt flip it twice
+        string theBeginning = array[i];
+        string theEnd = array[n-i-1]; // must take off 1 to account for first arr index being 0
+        array[i] = theEnd;
+        array[n-i-1] = theBeginning;
+    }
+    int returnValue = n/2; // counting on integer division to truncate
+    return returnValue;
+}
+
+int moveToEnd( string array[ ], int  n, int pos ){
+    int returnValue = -1;
+        if (pos < n - 1 && pos > 0){
+            string rememberStringAtPos = array[pos];
+            int whicheverPos = pos;
+            for (int i = pos; i < n - 1 ;i++){
+                array[whicheverPos] = array[whicheverPos + 1];
+                whicheverPos++;
+            }
+            
+            array[n-1] = rememberStringAtPos;
+        }
+        
+        if (pos >= 0 && pos <= n-1){
+            returnValue = pos;
+        }
+        
+        return returnValue;
+}
+
+
+int main() {
+    
+        string array[6] = {"Dennis", "Nat", "Chris", "Sari", "Jalal","Johnny"};
+        string array2[5] = {"Dennis", "Dennis", "Chris", "Sari", "Jalal"};
+        string array3[5] = {"0", "1", "2", "3", "4"};
+        string array4[5] = {"0", "1", "1", "3", "1"};
+        string punctArr[4] = {"1","31'",".com", "-/:90"};
+        
+        assert(locateMaximum(array, 5) == 3);
+        assert(locateMaximum(array, 4) == 3);
+        assert(locateMaximum(array, 1) == 0);
+        assert(locateMaximum(array, 2) == 1);
+        assert(hasNoDuplicates(array, 5) == 1);
+        assert(hasNoDuplicates(array2, 5) == 0);
+        assert(hasNoDuplicates(array2, 3) == 0);
+        assert(hasNoDuplicates(array2, 1) == 1);
+        assert(countXPairs(array3, 5, 2) == 1);
+        assert(countXPairs(array3, 5, 15) == 0);
+        assert(countXPairs(array3, 5, 3) == 2);
+        assert(findSecondToLastOccurrence(array2,5,"Dennis") == 0);
+        assert(findSecondToLastOccurrence(array,5,"Dennis") == -1);
+        assert(findSecondToLastOccurrence(array3,4,"0") == -1);
+        assert(findSecondToLastOccurrence(array3,5,"") == -1);
+        assert(findSecondToLastOccurrence(array4,5,"1") == 2);
+        assert(findSecondToLastOccurrence(array4,4,"1") == 1);
+        assert(findSecondToLastOccurrence(array4,-1,"1") == -1);
+        assert(findSecondToLastOccurrence(array,5,"Nat") == -1);
+        assert(countPunctuation(array,5) == 0);
+        assert(countPunctuation(array,5) == 0);
+        assert(countPunctuation(punctArr,4) == 5);
+        assert(moveToEnd(array, 5, 3) == 3);
+        assert(moveToEnd(array, 5, 6) == -1);
+        assert(flipAround(array,5) == 2);
+        assert(flipAround(array,6) == 3);
+        assert(flipAround(punctArr,4) == 2);
+        assert(flipAround(array,2) == 1);
+    std::cout << "Hello everybody, enjoy my project! ";
+
+    return 0;
+}
